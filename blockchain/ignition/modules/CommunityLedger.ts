b0b47cb11f60175f6970a1a0bc0a16725cff764f
lucianoZgabriel <lucianozanin@gmail.com>
2024-12-09 17:23:23 -0400

    chore: add script to deploy chore: configure hardhat.config to deploy chore: add .env example

--- blockchain/ignition/modules/CommunityLedger.ts
+++ blockchain/ignition/modules/CommunityLedger.ts
@@ -2,7 +2,15 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const CommunityLedgerModule = buildModule("CommunityLedgerModule", (m) => {
   const communityLedger = m.contract("CommunityLedger");
-  return { communityLedger };
+
+  const communityLedgerAdapter = m.contract("CommunityLedgerAdapter");
+
+  m.call(communityLedgerAdapter, "setImplementation", [communityLedger]);
+
+  return {
+    communityLedger,
+    communityLedgerAdapter,
+  };
 });
 
 export default CommunityLedgerModule;
